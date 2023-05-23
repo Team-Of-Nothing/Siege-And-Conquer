9b6f8d4fd1b2e0e3087feb05e69e2af6f05fac5a
xardas25 <240670@edu.p.lodz.pl>
2023-05-23 17:11:30 +0200

    Added Tenant and Gold v2

--- src/core/src/com/mygdx/game/Gold.java
+++ src/core/src/com/mygdx/game/Gold.java
@@ -1,2 +1,30 @@
-package com.mygdx.game;public class Gold {
+package com.mygdx.game;
+
+public class Gold {
+        private int amount;
+        private  int bonus;
+
+        public Gold(int amount) {
+            this.amount = amount;
+            this.bonus = 1;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void setBonus(int bonus) {
+            this.bonus = bonus;
+        }
+
+        public void addGold() {
+            this.amount *= bonus;
+        }
+
+        public void subtractGold(int amount) {
+            if (this.amount >= amount) {
+                this.amount -= amount;
+            }
+            System.out.println("Not enough gold");
+        }
 }
