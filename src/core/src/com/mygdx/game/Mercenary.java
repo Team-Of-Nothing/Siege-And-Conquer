945f20b7e0fce9537a977d703213115c2ad1755f
xardas25 <240670@edu.p.lodz.pl>
2023-05-30 23:44:13 +0200

    Uzupełnienie Mercenary i zmiana assetów

--- src/core/src/com/mygdx/game/Mercenary.java
+++ src/core/src/com/mygdx/game/Mercenary.java
@@ -1,35 +1,136 @@
 package com.mygdx.game;
 
-public class Mercenary {
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
-    private String name;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Mercenary extends Actor {
+
+    private int id;
     private int speed;
     private int attack;
     private int defense;
 
-    public Mercenary(String name, int speed, int attack, int defense) {
-        this.name = name;
-        this.speed = speed;
-        this.attack = attack;
-        this.defense = defense;
+    private int action;
+
+    private Image img;
+
+    private File mp3_attack;
+    private File mp3_death;
+
+    public Mercenary (int id) {
+        this.id = id;
+        this.setStats(id);
     }
 
-    public String getName() {
-        return name;
+    public File getMp3_attack() {
+        return mp3_attack;
+    }
+    public File setMp3(File mp3) {
+        return this.mp3_attack = mp3;
+    }
+    public File getMp3_death() {
+        return mp3_death;
+    }
+    public File setMp3_death(File mp3) {
+        return this.mp3_death = mp3;
+    }
+    public Image getImg() {
+        return img;
+    }
+    public Image setImg(Image img) {
+        return this.img = img;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getSpeed() {
         return speed;
     }
 
-    public void setSpeed(int speed) {
-        this.speed = speed;
+    public void setStats(int id){
+        this.mp3_attack = new File("core/assets/"+id+"/attack.mp3");
+        this.mp3_death = new File("core/assets/death.mp3");
+        switch (id)
+        {
+            case 1:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 2:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 3:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 4:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 5:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 6:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 7:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 8:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 9:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            case 10:
+                this.speed = 2;
+                this.attack = 1;
+                this.defense = 50;
+                break;
+            default:
+                break;
+        }
+    }
+    public void Action(int action) throws IOException {
+        File file;
+        BufferedImage Image;
+        getId();
+        file = new File("core/assets/"+id+action+".png");
+        Image = ImageIO.read(file);
+        this.img = Image;
     }
 
+    public void setSpeed(int speed) {
+            this.speed = speed;
+
+    }
     public int getAttack() {
         return attack;
     }
@@ -45,6 +146,8 @@ public class Mercenary {
     public void setDefense(int defense) {
         this.defense = defense;
     }
+
+
 
 
     public void attack(Mercenary enemy) {
